957bb3331f5fa9076a2d81816ab8f3f1b945e29b
ranaahmed <rnaahmd240@gmail.com>
2025-09-12 21:03:41 +0300

    END OF DAY2 UPDATE

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+//  copied from mzCode01
+document.addEventListener("DOMContentLoaded", () => {
+  const highlight = document.querySelector(".highlight");
+  const items = document.querySelectorAll(".navigation li");
 
+  const setHighlight = (item) => {
+    highlight.style.width = item.offsetWidth + "px";
+    highlight.style.left = item.offsetLeft + "px";
+  };
+
+
+  const activeItem = document.querySelector(".navigation li.active") || items[0];
+  setHighlight(activeItem);
+
+  items.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      items.forEach((el) => el.classList.remove("active"));
+      item.classList.add("active");
+      setHighlight(item);
+    });
+//  im traying to make the logo move here "note for me"
+    item.addEventListener("mouseenter", () => {
+      if (!item.classList.contains("active")) {
+        highlight.style.width = item.offsetWidth + "px";
+        highlight.style.left = item.offsetLeft + "px";
+      }
+    });
+
+    item.addEventListener("mouseleave", () => {
+      const activeItem = document.querySelector(".navigation li.active");
+      setHighlight(activeItem);
+    });
+  });
+
+  window.addEventListener("resize", () => {
+    const activeItem = document.querySelector(".navigation li.active");
+    setHighlight(activeItem);
+  });
+});
