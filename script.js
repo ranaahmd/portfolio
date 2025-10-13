518adc8eba68db2e452b35691f58b38be2969e4f
ranaahmed <rnaahmd240@gmail.com>
2025-10-13 10:31:30 +0300

    change css

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
-//  copied from mzCode01
-document.addEventListener("DOMContentLoaded", () => {
-  const highlight = document.querySelector(".highlight");
-  const items = document.querySelectorAll(".navigation li");
+const menu = document.getElementById("menu");
+const closeButton = document.getElementById("close-mobile");
+const nav = document.getElementById("nav-mobile");
+const navLink = document.querySelectorAll(".nav-link");
+const btn = document.getElementById('contact-btn');
+const icons = document.getElementById('social-icons');
+btn.addEventListener('click', () => {
+  icons.style.display = icons.style.display === 'none' ? 'flex' : 'none';
+  document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+});
+menu.addEventListener("click", () => {
+  nav.classList.add("show");
+});
 
-  const setHighlight = (item) => {
-    highlight.style.width = item.offsetWidth + "px";
-    highlight.style.left = item.offsetLeft + "px";
-  };
+closeButton.addEventListener("click", () => {
+  nav.classList.remove("show");
+});
 
-
-  const activeItem = document.querySelector(".navigation li.active") || items[0];
-  setHighlight(activeItem);
-
-  items.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      e.preventDefault();
-
-      items.forEach((el) => el.classList.remove("active"));
-      item.classList.add("active");
-      setHighlight(item);
-    });
-//  im traying to make the logo move here "note for"
-    item.addEventListener("mouseenter", () => {
-      if (!item.classList.contains("active")) {
-        highlight.style.width = item.offsetWidth + "px";
-        highlight.style.left = item.offsetLeft + "px";
-      }
-    });
-
-    item.addEventListener("mouseleave", () => {
-      const activeItem = document.querySelector(".navigation li.active");
-      setHighlight(activeItem);
-    });
-  });
-
-  window.addEventListener("resize", () => {
-    const activeItem = document.querySelector(".navigation li.active");
-    setHighlight(activeItem);
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
   });
 });
